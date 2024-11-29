20a2a60b0846ad37c3d2a4eac4a739f0f3bbc28a
g24force <102439759+g24force@users.noreply.github.com>
2024-11-29 12:44:48 +0000

    WIP

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: g24force <g24force@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 16:06:45 by gjose-fr          #+#    #+#             */
-/*   Updated: 2024/11/28 17:33:21 by g24force         ###   ########.fr       */
+/*   Updated: 2024/11/29 12:41:50 by g24force         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*set_line(char *buf)
+/* char	*set_line(char *buf)
 {
 	int	i;
 
@@ -72,18 +72,49 @@ int	main(void)
 	}
 	close(fd);
 	return (0);
+} */
+
+int	ft_has_newline(char * str)
+{
+	if (ft_strchr(str, '\n') == NULL)
+		return (0);
+	else
+		return (1);
 }
 
+void	ft_fill_stash(char *str, char *stash)
+{
+	//realocar a memoria do stash
+	//concat str ao stash
+}
+
+void	ft_fill_line(char *stash)
+{
+	//posso alocar aqui a memoria, ou recebe a variavel já como parametro
+	ft_strdup()
+	//stash
+	//concat str ao stash
+}
 
 char	*get_next_line(int fd)
 {
-	char	*s
+	char		*buf;
+	static char	*stash;
 	//verificar se está tudo bem com o fd
 	if (fd < 0)
 		return (NULL);
+	buf = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char)); //verificar
+	stash = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char)); //que size para o stash?
 	//ler os 5 bytes do file e passar para o stash
 	//ler a variavel buf e ver se tem um \n
 		//se não, ler os 5 bytes seguintes
+	while (!ft_has_newline(buf))
+	{
+		read(fd, buf, BUFFER_SIZE);
+		ft_fill_stash(buf, stash);
+	}
+	//implementar a realloc para o stash e return_var?
+	ft_fill_line();
 	//se tiver \n, copiar o conteúdo do stash para uma variavel, mas só até ao ao \n
 	//retornar a string
 }
